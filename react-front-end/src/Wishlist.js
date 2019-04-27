1bcf6528b029abaa595ef7beeb687ee4f30edc1e
Xia Dong <dongxiapp@gmail.com>
2019-04-27 06:33:01 +0000

    render wishlists from database

--- react-front-end/src/Wishlist.js
+++ react-front-end/src/Wishlist.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Product from './Product.js';
 // import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
 export default class Wishlist extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      // message: 'Click the button to load data!'
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     // message: 'Click the button to load data!'
+  //   }
+  // }
 
-  fetchData = () => {
-    axios.get('/') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log("success", response.data) // The entire response from the Rails API
+  // fetchData = () => {
+  //   axios.get('/') // You can simply make your requests to "/api/whatever you want"
+  //   .then((response) => {
+  //     // handle success
+  //     console.log("success", response.data) // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+  //     console.log(response.data.message) // Just the message
+  //     this.setState({
+  //       message: response.data.message
+  //     });
+  //   }) 
+  // }
 
   render() {
     return (
 
         <div className="wishlists">
-        <button onClick={this.fetchData} >
-          fetchData
-        </button>
+          {this.props.wishlist.name}
         <Product />     
         </div>
 
