43a646611e6fc0a5827e43262cb7433987821389
Xia Dong <dongxiapp@gmail.com>
2019-04-27 17:24:09 +0000

    add route and form to post new wishlist

--- rails-back-end/config/routes.rb
+++ rails-back-end/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # root to: 'wishlists#index'
+  root to: 'wishlists#index'
   namespace :api do
-    # get '/data', to: 'tests#index'
     get '/wishlists', to: 'wishlists#index'
-    #resources :wishlists, only: [:index, :show, :create]
+    post '/wishlists', to: 'wishlists#index'
   
   end
 end
--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import Wishlist from './Wishlist.js';
-// import Navbar from './Navbar.js';
+import Navbar from './Navbar.js';
 
 class App extends Component {
   constructor(props) {
@@ -12,24 +12,45 @@ class App extends Component {
     }
   }
 
+  addWishlist = e => {
+    e.preventDefault()
+    console.log(e.target.elements.newWishlist.value)
+    // const formData = new FormData()
+    // formData.set('name', 'Fred')
+    axios.post('/api/wishlists', {newWishlsitName: e.target.elements.newWishlist.value})
+    .then((response) => {
+      console.log(response)
+    }).catch(function (error) {
+      // handle error
+      console.log(error)
+    }) 
+  }
+
   componentDidMount() {
-    // fetchData = () => {
-      axios.get('/api/wishlists') // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        // console.log(response.data)// The entire response from the Rails API
-        // console.log(response.data.wishlists.length) // Just the message
-        this.setState({
-          wishlists: this.state.wishlists.concat(response.data.wishlists)
-        });
-      }) 
-    // }
+    axios.get('/api/wishlists') // load all the wishlists from rails api
+    .then((response) => {
+      // handle success
+      // console.log(response.data)// The entire response from the Rails API
+      // console.log(response.data.wishlists.length) // Just the message
+      this.setState({
+        wishlists: this.state.wishlists.concat(response.data.wishlists)
+      })
+    }).catch(function (error) {
+      // handle error
+      console.log(error)
+    }) 
   }
 
   render() {
     return (
       <div className="App">
+        < Navbar />
         <h1>My wishlists</h1>
+        Add new wishlist 
+        <form onSubmit={this.addWishlist}>
+          <input name="newWishlist" placeholder="Type a message and hit ENTER" />  
+          <button type="submit">Add</button>
+        </form>
         {this.state.wishlists.map(wishlist => (
           <Wishlist key={wishlist.id} wishlist={wishlist}/>
         ))}
