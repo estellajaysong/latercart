07358ddd431a8cdcc29e11b57015caaf3eee1d77
Bonita Choi <bonitac93@gmail.com>
2019-04-28 20:12:41 +0000

    added rspec to gemfile and generated a basic test for submit button clicked feature

---
+++ rails-back-end/spec/features/add_product_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Chrome extension submit button clicked", type: :feature, js: true do
+  scenario "adds product to wishlist" do
+    # visit "/"
+
+    # save_screenshot
+  end
+end
+
+#rspec spec/features
+
+# for generating: bin/rails generate rspec:feature add_product
+# do for model (bin/rails generate rspec:model product)
