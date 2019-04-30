e25acb58809d55872a74e93428c43a29e81fbe5e
Bonita Choi <bonitac93@gmail.com>
2019-04-30 20:20:44 +0000

    product component shows for the correct wishlists

--- react-front-end/src/Product.js
+++ react-front-end/src/Product.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Wishlist from './Wishlist.js'
 
@@ -12,18 +11,19 @@ export default class Product extends Component {
     }
   }
 
-  componentDidMount(){
-      axios.get(`/api/products/${this.state.product.id}`) // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        console.log(response.data) // The entire response from the Rails API  
-        this.setState({
-          product: response.data
-        });
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+  // componentDidMount(){
+  //     axios.get(`/api/products/${this.props.product.id}`) // You can simply make your requests to "/api/whatever you want"
+  //     .then((response) => {
+  //       console.log(response.data) // The entire response from the Rails API  
+  //       this.setState({
+  //         product: response.data
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.log("here")
+  //       console.log(err)
+  //     })
+  // }
 
   
 
@@ -32,11 +32,11 @@ export default class Product extends Component {
       <Router>
       <div className="product">
       <div>
-      <Link to="/product">Product</Link>
+      <Link to="/product/:productId">Product</Link>
       <h4>{this.props.product.name}</h4>
       <img className="product-img" src={this.props.product.img_url} alt={this.props.product.name} />
       </div>
-      <Route path="/product" component={Product} />
+      <Route path="/product/:productId" component={Product} />
       <div>
       <p>Price: ${this.props.product.price}</p>
       <p>Rating: {this.props.product.rating}</p>
