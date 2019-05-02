ddf470658f5e206636f33b298f7fd5286b45494e
Xia Dong <dongxiapp@gmail.com>
2019-05-02 00:08:51 +0000

    send email after use shares a wishlist to an email

--- rails-back-end/app/controllers/api/user_wishlists_controller.rb
+++ rails-back-end/app/controllers/api/user_wishlists_controller.rb
@@ -9,11 +9,11 @@ class Api::UserWishlistsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:info][:email])
-    puts "share: user.id>>>>>>>>>>>>#{@user.id}"
-    @user.user_wishlists.create({
+    @mailUser = User.find_by(email: params[:info][:email])
+    @mailUser.user_wishlists.create({
       wishlist_id: params[:info][:wishlist_id]
     }) 
+    UserMailer.share_wishlist_email(@mailUser, current_user).deliver_now
     # render json: @newUserWishlist
   end
 
