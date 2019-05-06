a590a2e7362ecfa1d94e483b561e2ce226b865df
Xia Dong <dongxiapp@gmail.com>
2019-05-06 21:21:38 +0000

    change latercard font weight

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -24,7 +24,7 @@ const theme = createMuiTheme({
     "fontFamily": "\"Montserrat\", \"ABeeZee\", sans-serif",
     "fontWeightLight": 300,
     "fontWeightRegular": 400,
-    "fontWeightMedium": 900
+    "fontWeightMedium": 700
    }
 });
 
